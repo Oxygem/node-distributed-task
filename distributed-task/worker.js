ba81a530451e92620c98a8291a4e62b0c55da1d5
Nick Barrett <nick@oxygem.com>
2014-06-21 02:26:25 +0100

    mostly laid out, re-thought distributor connection loop, new logging

--- distributed-task/worker.js
+++ distributed-task/worker.js
@@ -15,16 +15,12 @@ var tls = require('tls'),
 
 var Worker = function() {
     events.EventEmitter.call(this);
-    this.log_prefix = '[Worker]: ';
+    this.log_prefix = '[Worker] ';
     this.redis_up = false;
 
     this.task_definitions = {};
     this.distributors = {};
     this.tasks = {};
-
-    this.stopDistributorTasks = function(distributor) {
-        utils.log.call(this, 'stopping tasks for distributor: ' + distributor.hostname);
-    };
 
     this.init = function(config) {
         var self = this;
@@ -34,7 +30,7 @@ var Worker = function() {
         this.debug_netev = config.debug_netev || false;
         if(this.debug) {
             utils.log.call(self, 'debug enabled');
-            utils.log.call(self, 'config dump: ', config);
+            utils.log.call(self, 'config dump', false, config);
         }
 
         // Start the monitoring loop
@@ -46,23 +42,48 @@ var Worker = function() {
 
     this.addTask = function(task_name, object) {
         this.task_definitions[task_name] = object;
+        utils.log.call(this, 'task added', task_name);
+    };
+
+    this.stopDistributorTasks = function(distributor) {
+        utils.log.call(this, 'stopping tasks for distributor', distributor.hostname);
+    };
+
+    this.getTasks = function() {
+        return this.tasks;
+    };
+
+    this.stopTask = function(task_id) {
+
+    };
+
+    this.stopAllTasks = function(task_id) {
+
     };
 
     this.onAddTask = function(task_id) {
-        utils.log.call(this, 'task added: ' + task_id);
+        utils.log.call(this, 'task added', task_id);
 
         // Get task from Redis
 
         // Create new object from task_definitions
 
-        // Subscribe to its events
+        // Subscribe to its _events
     };
 
     var _onRedisDown = function(err) {
         if(this.redis_up) {
-            utils.error.call(this, 'Redis is down', err);
             this.redis_up = false;
             this.redis = null;
+
+            // For each distributor, send redisDown notification
+            for(var key in this.distributors) {
+                var dist = this.distributors[key];
+                if(!dist || dist === true) continue;
+
+                dist.emit('redisDown');
+            }
+            utils.error.call(this, 'Redis is down', err);
         }
     };
 
@@ -73,10 +94,10 @@ var Worker = function() {
         // Send distributor healthchecks
         for(var key in this.distributors) {
             var dist = this.distributors[key];
-            if(!dist) continue;
+            if(!dist || dist === true) continue;
 
             dist.emit('healthCheck', os.loadavg()[1]);
-            utils.log.call(this, 'sending healthcheck to: ' + dist.hostname);
+            utils.log.call(this, 'sending healthcheck', dist.hostname);
         }
 
         // Check Redis
@@ -89,8 +110,16 @@ var Worker = function() {
             });
             redis_client.on('ready', function() {
                 if(!self.redis_up) {
-                    utils.log.call(self, 'connected to Redis');
                     self.redis_up = true;
+
+                    // For each distributor, send redisUp notification
+                    for(var key in self.distributors) {
+                        var dist = self.distributors[key];
+                        if(!dist || dist === true) continue;
+
+                        dist.emit('redisUp');
+                    }
+                    utils.log.call(self, 'connected to Redis');
                 }
             });
             redis_client.on('error', function(err) {
@@ -105,58 +134,77 @@ var Worker = function() {
             }
 
             // Check disconnected distributors
+            // this.distributors:
+            //      true = connecting
+            //      false = connection error
+            //      undefined = other error, requeue
+
             for(var i=0; i<config.distributors.length; i++) {
-                var distributor = config.distributors[i],
-                    hostname = distributor[0] + ':' + distributor[1];
+                (function(i) {
+                    var dist_config = config.distributors[i],
+                        hostname = dist_config[0] + ':' + dist_config[1],
+                        distributor = self.distributors[hostname];
 
-                if(!this.distributors[hostname]) {
-                    if(this.distributors[hostname] === undefined) {
-                        this.distributors[hostname] = false;
-                        utils.log.call(self, 'connecting to distributor: ' + hostname);
+                    if(distributor === undefined || distributor === false) {
+                        if(distributor === undefined)
+                            utils.log.call(self, 'connecting to distributor', hostname);
+
+                        // Connecting state
+                        self.distributors[hostname] = true;
+                        var connection = tls.connect({
+                            host: dist_config[0],
+                            port: dist_config[1],
+                            rejectUnauthorized: false
+                        });
+
+                        var stream = connection.on('secureConnect', function() {
+                            // Distributor expects share key immediately
+                            stream.write(self.share_key);
+
+                            // Sent upon correct share_key, dist already netev wrapped
+                            utils.receiveUntil(stream, 'HELLO WORKER', function(stream) {
+                                // Wrap  the stream for incoming events
+                                var dist = netev(stream, self.debug_netev);
+                                dist.hostname = hostname;
+                                self.distributors[hostname] = dist;
+
+                                // Immediately emit hostname, we are ready for tasks!
+                                dist.emit('hostname', os.hostname());
+                                self.emit('distributorConnected', dist_config);
+
+                                // Receive tasks
+                                dist.on('addTask', function(task_id) {
+                                    self.onAddTask(task_id);
+                                });
+
+                                utils.log.call(self, 'distributor added', dist.hostname);
+                            }, {timeout: 10});
+                        });
+
+                        connection.on('end', function() {
+                            var distributor = self.distributors[hostname];
+
+                            // Not true or false (not connecting)
+                            if(typeof distributor != 'boolean') {
+                                utils.log.call(self, 'distributor disconnected', hostname);
+                                self.stopDistributorTasks(distributor);
+                                delete self.distributors[hostname];
+                            }
+                        });
+                        connection.on('error', function(err) {
+                            var distributor = self.distributors[hostname];
+
+                            // Connect failure
+                            if(distributor === true) {
+                                setTimeout(function() { self.distributors[hostname] = false; }, self.backoff || 10000);
+                            } else {
+                                utils.log.call(self, 'distributor error', hostname, err);
+                                self.stopDistributorTasks(distributor);
+                                delete self.distributors[hostname];
+                            }
+                        });
                     }
-
-                    var connection = tls.connect({
-                        host: distributor[0],
-                        port: distributor[1],
-                        rejectUnauthorized: false
-                    });
-
-                    var stream = connection.on('secureConnect', function() {
-                        // Distributor expects share key immediately
-                        stream.write(self.share_key);
-
-                        // Sent upon correct share_key, dist already netev wrapped
-                        utils.receiveUntil(stream, 'HELLO WORKER', function(stream) {
-                            // Wrap  the stream for incoming events
-                            var dist = netev(stream, self.debug_netev);
-                            dist.hostname = hostname;
-                            self.distributors[hostname] = dist;
-
-                            // Immediately emit hostname, we are ready for tasks!
-                            dist.emit('hostname', os.hostname());
-                            self.emit('distributorConnected', distributor);
-
-                            // Receive tasks
-                            dist.on('addTask', function(task_id) {
-                                self.onAddTask(task_id);
-                            });
-
-                            utils.log.call(self, 'distributor added: ' + dist.hostname);
-                        }, {timeout: 10});
-                    });
-
-                    connection.on('end', function() {
-                        utils.log.call(self, 'distributor disconnected: ' + hostname);
-
-                        self.stopDistributorTasks(self.distributors[hostname]);
-                        delete self.distributors[hostname];
-                    });
-                    connection.on('error', function(err) {
-                        if(self.distributors[hostname] === undefined) {
-                            utils.error.call(self, 'worker down', err);
-                        }
-                    });
-                }
+                })(i);
             }
         }
     };
